8066bf5eebccf33a6999f501123ce87c2e211d1a
mohieey <Mohhiey@gmail.com>
2023-12-04 12:37:00 +0200

    created controllers package

--- main.go
+++ main.go
@@ -2,45 +2,36 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/mohieey/lenslocked/controllers"
 	"github.com/mohieey/lenslocked/views"
 )
-
-func excuteTemplate(w http.ResponseWriter, filePath string) {
-	t, err := views.Parse(filePath)
-	if err != nil {
-		log.Printf("Error parsing %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	t.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	excuteTemplate(w, "home")
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	excuteTemplate(w, "contact")
-
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	excuteTemplate(w, "faq")
-
-}
 
 var port = ":3000"
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+
+	tmpl, err := views.Parse("home")
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tmpl))
+
+	tmpl, err = views.Parse("contact")
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tmpl))
+
+	tmpl, err = views.Parse("faq")
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tmpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Not Found", http.StatusNotFound)
 	})
