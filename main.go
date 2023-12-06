e87d8919244988ded383b6df2384ddf2f6a189c8
mohieey <Mohhiey@gmail.com>
2023-12-06 10:52:04 +0200

    added dynamic and named templates

--- main.go
+++ main.go
@@ -15,13 +15,13 @@ var port = ":3000"
 func main() {
 	r := chi.NewRouter()
 
-	tmpl := views.Must(views.ParseFS(templates.FS, "home.gohtml", "layout_parts.gohtml"))
+	tmpl := views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))
 	r.Get("/", controllers.StaticHandler(tmpl))
 
-	tmpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml", "layout_parts.gohtml"))
+	tmpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))
 	r.Get("/contact", controllers.StaticHandler(tmpl))
 
-	tmpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml", "layout_parts.gohtml"))
+	tmpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))
 	r.Get("/faq", controllers.FAQ(tmpl))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
